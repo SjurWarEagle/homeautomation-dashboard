5a04d15d4596ea1b6a6bdb4cadfbaef6d04f832f
Torsten Kunkel <git@tkunkel.de>
2020-12-07 13:46:28 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, ViewChild} from '@angular/core';
+import {IMqttMessage, MqttService} from 'ngx-mqtt';
+import {Subscription} from 'rxjs';
+import NoSleep from 'nosleep.js';
+
+// tslint:disable-next-line
+var noSleep = new NoSleep();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'homeautomation-dashboard';
+
+export class AppComponent implements OnDestroy {
+  @ViewChild('startButton')
+  private startButton: any;
+
+  private subscription: Subscription;
+  public co2Value: number;
+  public mainBackground = 'black';
+  public background = 'gray';
+  public started: boolean;
+
+  constructor(private mqttService: MqttService) {
+    this.subscription = this.mqttService.observe('home/livingroom/co2/value').subscribe((message: IMqttMessage) => {
+      // this.message = message.payload.toString();
+      // console.log('message', this.message);
+      this.co2Value = +message.payload.toString();
+      this.background = 'red';
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  public start(): void {
+    noSleep.enable();
+    this.started = true;
+    // go fullscreen, only active for chrome based
+    this.startButton.nativeElement.webkitRequestFullscreen();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {IMqttServiceOptions, MqttModule} from 'ngx-mqtt';
 
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '192.168.73.58',
+  port: 9001,
+  protocol: 'ws',
+  path: '/mqtt'
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
   ],
   providers: [],
   bootstrap: [AppComponent]
